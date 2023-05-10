da5a3e75c503220663658e1d757c6f46be50eb56
ramiroquiro <rama.exe.13@gmail.com>
2023-05-09 23:43:21 -0300

    logrando mascara de capa

---
+++ src/Tarjeta1/component/HeroTarjeta.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useStoraStyle } from "@/context/DiseñoUser/contextStyles";
+
+export default function HeroTarjeta() {
+  const { color } = useStoraStyle((state) => ({
+    color: state.color,
+  }));
+  return (
+    <>
+      {/* <div
+    style={{
+      clipPath: "url(#trazado)",
+      backgroundImage: `linear-gradient(0deg,${color.color1} 0%,${color.color2} 80%) `,
+      //   mixBlendMode: "overlay",
+    }}
+    className="absolute top-0   left-0 w-full h-full bg-gradient-to-t  from-primary-100 to-primary-500 "
+  ></div> */}
+      <svg
+        className={`absolute top-0   left-0 w-full drop-shadow-lg h-full`}
+        version="1.2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1080 720"
+        width="1080"
+        height="720"
+      >
+      <path id="Forma 1" class="s0" d="m-96 428.9c0 0 96 78 174 51 48-16.6 68-93.3 198-99 56.4-2.5 170.2 22.3 255 48.1 168.9 51.3 313.7 75.4 379.5 67.4 68.6-8.4 250.5-55.2 250.5-112.5 0-241.7-1.5-383.9-1.5-383.9h-1251l-10.5 419.9zm-1 0.1"/>
+      </svg>
+      <div
+        style={{
+          backgroundImage: `linear-gradient(0deg,${color.color1} 0%,${color.color2} 80%) `,
+        }}
+        className="absolute top-0 mix-blend-screen   left-0 w-full h-full bg-gradient-to-t  from-primary-100 to-primary-500 "
+      ></div>
+    </>
+  );
+}
--- src/app/dashboard/Disenio/BotonesSeleccionadoresColores.jsx
+++ src/app/dashboard/Disenio/BotonesSeleccionadoresColores.jsx
@@ -4,17 +4,16 @@ import { useState } from "react";
 import { ChromePicker, SketchPicker } from "react-color";
 
 export default function BotonesSeleccionadoresColores({name}) {
-  const [background, setBackground] = useState({
-    color1: "#c5c5c5",
-    color2: "#e1e1e1",
-  });
-
-  const guardarColores=useStoraStyle((state)=>state.guardarColores)
+ 
 
   const [open, setOpen] = useState(false);
+  const guardarColores=useStoraStyle((state)=>state.guardarColores)
+
+  const {color}=useStoraStyle((state)=>({
+color:state.color
+  }))
 
   const handleChangeComplete = (background) => {
-    setBackground({[name]: background.hex });
     guardarColores(name,background.hex)
   };
   return (
@@ -22,15 +21,14 @@ export default function BotonesSeleccionadoresColores({name}) {
       name={name}
       onClick={() => setOpen(!open)}
       style={{
-        backgroundColor: background[name],
+        backgroundColor: color[name],
       }}
-      className={`w-8 h-8 rounded-lg cursor-pointer group  `}
+      className={`w-8 h-8 border shadow-md rounded-lg cursor-pointer group  `}
     >
       {open && (
         <SketchPicker
-          className="mt-5 ml-10 animate-[aparecer_.5s]"
-          color={background}
-          
+          className="mt-10 ml-10 animate-[aparecer_.5s]"
+          color={color}
           onChange={handleChangeComplete}
         //   onChangeComplete={handleChangeComplete}
         />
