3979f78a0ca3cd1bcfae2d146381681e69b7a565
ramiroquiro <rama.exe.13@gmail.com>
2023-06-16 20:01:18 -0300

    creacion de la seccion empresa en la tarjeta publicaa

--- src/app/[userName]/Tarjeta1/component/SectionTarjetaSola.jsx
+++ src/app/[userName]/Tarjeta1/component/SectionTarjetaSola.jsx
@@ -16,7 +16,7 @@ export default function SectionTarjetaSola({}) {
   return (
    
     <section className="bg-white border   rounded-lg overflow-hidden lg:w-[25vw] z-20  md:w-[35vw] w-full md:h-[95vh]  shadow-shadowCaja1 relative h-[98vh]  flex flex-col items-center justify-between ">
-        <span className="absolute top-3 z-30 text-gray-200 text-sm text-thin"> RamiroCode | Web Developer </span>
+        <span className="absolute top-3 z-30 text-gray-800 text-sm text-thin"> RamiroCode | Web Developer </span>
        {/* <HomeTarjeta />  */}
 
        <RenderPaginas name={selectBoton} />
--- src/components/SectionOutlet.jsx
+++ src/components/SectionOutlet.jsx
@@ -4,10 +4,10 @@ import NavBar from "./NavBar/NavBar";
 
 export default function SectionOutlet({ children }) {
   return (
-    <div className="w-full min-h-screen  bg-gray-100 py-12 ">
+    <div className="w-full min-h-screen  bg-gray-100 md:py-12 py-6 ">
       <Toaster/>
 <NavBar/>
-      <div className="md:w-10/12 w-11/12 mx-auto text-zinc-700 font-medium animate-[aparecer_1s] flex  flex-col justify-center gap-10 items-center md:pt-5 pt-24 md:pl-10">
+      <div className="md:w-10/12 relative w-11/12 mx-auto text-zinc-700 font-medium animate-[aparecer_1s] flex  flex-col justify-center md:gap-10 items-center md:pt-5 pt-24 md:pl-10">
         {children}
       </div>
     </div>
