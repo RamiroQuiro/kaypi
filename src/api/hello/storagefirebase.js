cfdaab80690e242adb25b131e2fc48bf3571ce98
ramiroquiro <rama.exe.13@gmail.com>
2023-06-18 20:10:56 -0300

    creacion de la conwezion dsel stroga de firebasea

--- src/api/hello/storagefirebase.js
+++ src/api/hello/storagefirebase.js
@@ -1,36 +1,27 @@
-const { ref } = require("firebase/storage");
-const { db, storage } = require("./firabase");
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { db, storage } from "./firabase";
+import { updateDoc } from "firebase/firestore";
+import { toast } from "react-hot-toast";
 
-
-const =()=>{
-    
-}
-
-const fileRef = ref(
-    storage,
-      `imagenes/${user?.uid}/${file.name}`
-    );
-    await uploadBytes(fileRef, file).then(async (uploadTask) => {
-      await getDownloadURL(fileRef).then(async (url) => {
-        //  setFileURL(url);
-        await updateDoc(docRef, {
-          [businessName]: arrayUnion({
-            nombre: fileName,
-            url: url,
-            posicion: filePosicion
-          }),
+const cargaImagenes = async (uid, file) => {
+  const docRef = doc(db, `usuarios/${uid}`);
+  const referencia = `imagenes/${uid}/${file.name}`;
+  const fileRef = ref(storage, referencia);
+  await uploadBytes(fileRef, file).then(async (uploadImg) => {
+    await getDownloadURL(fileRef).then(async (url) => {
+      await updateDoc(docRef, {
+        images: arrayUnion({
+          nombre: fileName,
+          url: url,
+        }),
+      })
+        .then(() => {
+          toast.success("Imagen subida correctamente");
         })
-          .then(() => {
-            toast.success("Imagen subida correctamente");
-            setFile(null);
-            setPreviewURL(null);
-            setFileURL(null);
-            setLoading(false);
-          })
-          .catch((error) => {
-            alert("Error al subir imagen, intente de nuevo");
-            console.log(error);
-          });
-      });
+        .catch((error) => {
+          alert("Error al subir imagen, intente de nuevo");
+          console.log(error);
+        });
     });
-  };
+  });
+};
